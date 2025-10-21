6e54edc39e89658483611c330498155853d85499
Rafael García Prieto <rafael.garcia.prieto.tech@bbva.com>
2025-10-21 13:35:14 +0200

    refactor: remove OAuth2 implementation and update GitHub backend configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import decapCmsOauth from "astro-decap-cms-oauth";
+
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
-  integrations: [
-    icon(),
-    sitemap({
-      filter: (page) => !page.includes("/admin"),
-      changefreq: "weekly",
-      priority: 0.7,
-    }),
-  ],
+  integrations: [icon(), sitemap({
+    filter: (page) => !page.includes("/admin"),
+    changefreq: "weekly",
+    priority: 0.7,
+  }), decapCmsOauth()],
 });
