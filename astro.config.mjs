13a69c8dee28db45cfd562e5ee9c9db08af5bbc0
Rafael García Prieto <rafael.garcia.prieto.tech@bbva.com>
2025-10-21 13:45:23 +0200

    chore: update dependencies and remove unused files
    - Added @astrojs/vercel dependency to package.json.
    - Deleted the admin.astro page as it is no longer needed.
    - Simplified vercel.json by removing function configurations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 export default defineConfig({
   site: "https://www.yourwebsite.com", // update me!
-  integrations: [icon(), sitemap({
-    filter: (page) => !page.includes("/admin"),
-    changefreq: "weekly",
-    priority: 0.7,
-  }), decapCmsOauth()],
+  output: "server",
+  adapter: vercel(),
+  integrations: [
+    icon(),
+    sitemap({
+      filter: (page) => !page.includes("/admin"),
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+    decapCmsOauth(),
+  ],
 });
